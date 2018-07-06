154ddfbaa430bc6a87ffe280effe9892fe33fbf5
yulifu <yulifu@douyu.tv>
2018-07-06 15:49:43 +0800

    change js

--- html2dom/src/index.js
+++ html2dom/src/index.js
@@ -135,13 +135,21 @@ XDom.prototype = {
  * 自闭和标签
  */
 XDom.selfClosingTags = {
+    meta: 1,
+    base: 1,
+    link: 1,
     hr: 1,
     br: 1,
+    wbr: 1,
     col: 1,
     img: 1,
-    textarea: 1,
+    area: 1,
     input: 1,
-    embed: 1
+    textarea: 1,
+    embed: 1,
+    param: 1,
+    source: 1,
+    object: 1
 };
 
 /**
